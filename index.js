a9e2c1f462ee5fe8c5ca85ace661e161214df223
noelrussomendez <mrussomendez@udesa.edu.ar>
2020-10-12 15:45:37 -0300

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let mensaje= "Bienvenidos a mi sitio!";
+alert(mensaje);
+
+let avanzar = "¿Queres avanzar?";
+let resultado = confirm (avanzar);
+let si ="si";
+let no = "no";
+if (avanzar == si) 
+{
+    alert(<h2>“¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!”.</h2>);
+} else (avanzar == no) 
+{
+    alert (<h2>“Lamentamos que no quieras continuar tu visita por este sitio increíble.”</h2>);
+}
